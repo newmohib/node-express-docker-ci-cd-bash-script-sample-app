a4da2e774d3a9f0b512dc8c664e03ee7c5cac03b
Mohibur Rahman <mohib@bs-23.net>
2023-08-01 12:37:44 +0600

    update scipt file for use image name as a variable

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 
 #### Make the 1st script executable by running the following command in the terminal:
-    ``` chmod +x run_app.sh ```
+    chmod +x run_app.sh
 
 ####  Install and Start the Docker
 #### Run the run_app.sh script to build Docker image and run the container:
-    ``` ./run_app.sh ```
+    npm run start:dev
 
--- run_app.sh
+++ run_app.sh
@@ -1,28 +1,31 @@
 #!/bin/bash
 
+# Declare and assign the value to the variable
+docker_image_name="nodejs-sample-app-1"
+
 # Function to build the Docker image
 build_image() {
   echo "Building the Docker image..."
-  docker build -t nodejs-sample-app-1:$1 --build-arg NODE_ENV=$1 .
+  docker build -t "$docker_image_name:$1" --build-arg NODE_ENV="$1" .
 }
 
 # Function to stop and remove the Docker container
 stop_and_remove_container() {
   echo "Stopping and removing the existing container..."
-  docker stop nodejs-sample-app-1
-  docker rm nodejs-sample-app-1
+  docker stop "$docker_image_name-container"
+  docker rm "$docker_image_name-container"
 }
 
 # Function to remove the Docker image
 remove_image() {
   echo "Removing the existing image..."
-  docker rmi nodejs-sample-app-1:$1
+  docker rmi "$docker_image_name:$1"
 }
 
 # Function to run the Docker container
 run_container() {
   echo "Running the Docker container..."
-  docker run -p 3000:3000 -v "$(pwd)":/app --name nodejs-sample-app-1 nodejs-sample-app-1:$1
+  docker run -p 3000:3000 -v "$(pwd)":/app --name "$docker_image_name-container" "$docker_image_name:$1"
 }
 
 # Function to show usage instructions
@@ -43,38 +46,32 @@ if [ "$1" != "dev" ] && [ "$1" != "prod" ]; then
 fi
 
 # Check if the Docker container exists and stop it if running
-if docker ps -a | grep -q "nodejs-sample-app-1:$1"; then
-  stop_and_remove_container $1
+if docker ps -a | grep -q "$docker_image_name-container"; then
+  if docker container stop "$docker_image_name-container"; then
+    docker container rm "$docker_image_name-container"
+  else
+    echo "Failed to stop and remove the existing container."
+    exit 1
+  fi
 fi
 
 # Check if the Docker image exists and remove it
-if docker image ls | grep -q "nodejs-sample-app-1:$1"; then
-  remove_image $1
+if docker image ls | grep -q "$docker_image_name:$1"; then
+  if docker image rm "$docker_image_name:$1"; then
+    echo "Removed the existing image."
+  else
+    echo "Failed to remove the existing image."
+    exit 1
+  fi
 fi
 
 # Build the Docker image
-build_image $1
-
-# Run the Docker container
-run_container $1
-
-
-# Build the Docker image based on the provided mode
-
-# if [ "$1" == "dev" ]; then
-#     echo "Started: Development"
-#     # Build the Docker image
-#     docker build -t nodejs-sample-app-1:dev --build-arg NODE_ENV=dev .
-
-#     # Run a container based on the built image with dev directory like "$(pwd)"
-#     docker run -p 3000:3000 -v "$(pwd)":/app nodejs-sample-app-1:$1 
-
-# else
-#     echo "Started: Production Mode"
-#     docker build -t nodejs-sample-app-1:prod --build-arg NODE_ENV=prod .
-
-#     # Run a container based on the built image
-#     docker run -p 3000:3000 nodejs-sample-app-1:$1
-    
-# fi
+if build_image "$1"; then
+  # Run the Docker container
+  run_container "$1"
+else
+  echo "Failed to build the Docker image. Using existing image and running the container..."
+  # Run the Docker container with the existing image
+  run_container "$1"
+fi
 
