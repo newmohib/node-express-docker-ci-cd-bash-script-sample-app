2a7cbbbb9b5cb86ab4cf80f4325b84f01141e458
mohibbs <mohib@bs-23.net>
2023-08-03 00:45:02 +0600

    docker build and run script is working fine and also backup but rollback is not working

--- run_app.sh
+++ run_app.sh
@@ -1,34 +1,67 @@
 #!/bin/bash
 
+# Declare and assign the value to the variable
+docker_image_name="nodejs-sample-app-1"
+temp_image_name="temp-nodejs-sample-app"
+temp_container_name="temp-nodejs-container"
+temp_volume_name="temp-nodejs-volume"
+
 # Function to build the Docker image
 build_image() {
   echo "Building the Docker image..."
-  docker build -t nodejs-sample-app-1:$1 --build-arg NODE_ENV=$1 .
+  docker build -t "$docker_image_name:$1" --build-arg NODE_ENV="$1" .
 }
 
 # Function to stop and remove the Docker container
 stop_and_remove_container() {
-  echo "Stopping and removing the existing container..."
-  docker stop nodejs-sample-app-1
-  docker rm nodejs-sample-app-1
+  echo "Stopping and removing the container..."
+  docker stop "$1"
+  docker rm "$1"
 }
 
 # Function to remove the Docker image
 remove_image() {
-  echo "Removing the existing image..."
-  docker rmi nodejs-sample-app-1:$1
+  echo "Removing the image..."
+  docker rmi "$docker_image_name:$1"
 }
 
 # Function to run the Docker container
 run_container() {
   echo "Running the Docker container..."
-  docker run -p 3000:3000 -v "$(pwd)":/app --name nodejs-sample-app-1 nodejs-sample-app-1:$1
+  docker run -p 3000:3000 -v "$(pwd)":/app --name "$1" "$docker_image_name:$2"
+}
+
+# Function to run the temporary Docker container with dynamically allocated host port
+run_temp_container() {
+  echo "Running the temporary Docker container with dynamically allocated host port..."
+  docker run -P -v "$(pwd)":/app --name "$temp_container_name" "$1"
+}
+
+# Function to create a volume to save the state of the app
+create_temp_volume() {
+  echo "Creating a temporary volume to save the state of the app..."
+  docker volume create "$temp_volume_name"
+}
+
+# Function to backup app data to the temporary volume
+backup_app_data() {
+  echo "Backing up app data to the temporary volume..."
+  docker run --rm -v "$(pwd)":/app -v "$temp_volume_name":/backup alpine cp -a /app /backup
+}
+
+# Function to restore app data from the temporary volume
+restore_app_data() {
+  echo "Restoring app data from the temporary volume..."
+  docker run --rm -v "$(pwd)":/app -v "$temp_volume_name":/backup alpine cp -a /backup/app /app
 }
 
 # Function to show usage instructions
 show_usage() {
   echo "Invalid argument. Usage: $0 [dev|prod]"
 }
+
+# Trap signals for cleanup
+trap 'cleanup' ERR
 
 # Check if an argument is provided
 if [ $# -ne 1 ]; then
@@ -42,35 +75,62 @@ if [ "$1" != "dev" ] && [ "$1" != "prod" ]; then
   exit 1
 fi
 
+
+# Start Run from Here
+
+
 # Check if the Docker container exists and stop it if running
-if docker ps -a | grep -q "nodejs-sample-app-1:$1"; then
-  # Try running the new image with a temporary container
-  echo "Trying to run the new image with a temporary container..."
-
-  if docker run --name temp-container nodejs-sample-app-1:$1; then
-    # Stop and remove the existing container
-    echo "Stop and remove the existing container..."
-    stop_and_remove_container $1
-    # Remove the temporary container
-    echo "Remove the temporary container..."
-    docker rm temp-container
-  else
-    # Remove the failed temporary container
-    echo "Remove the failed temporary container for failed to run..."
-    docker rm temp-container
-    echo "Failed to run the new image. Using existing image and container..."
-    exit 1
-  fi
-
+if docker ps -a | grep -q "$docker_image_name-container"; then
+  # Stop and remove the existing container
+  stop_and_remove_container "$docker_image_name-container"
 fi
 
 # Check if the Docker image exists and remove it
-if docker image ls | grep -q "nodejs-sample-app-1:$1"; then
-  remove_image $1
+if docker image ls -q "$docker_image_name:$1" >/dev/null; then
+  remove_image "$1"
 fi
 
-# Build the Docker image
-build_image $1
+# Check if the main container exists; if not, build and run it directly
+if ! docker ps -a | grep -q "$docker_image_name-container"; then
+  # Create a volume to save the state of the app
+  create_temp_volume
 
-# Run the Docker container
-run_container $1
+  # Backup app data to the temporary volume
+  backup_app_data
+
+  # Build the Docker image
+  if build_image "$1"; then
+    # Run the Docker container with the fixed port 3000
+    run_container "$docker_image_name-container" "$1"
+    exit 0
+  else
+    echo "Failed to build the Docker image."
+    # Restore app data from the temporary volume to rollback changes
+    restore_app_data
+    exit 1
+  fi
+fi
+
+# Create a volume to save the state of the app
+create_temp_volume
+
+# Backup app data to the temporary volume
+backup_app_data
+
+# Build the Docker image
+if build_image "$1"; then
+  # Run the temporary container to trigger the build
+  run_temp_container "$docker_image_name:$1"
+else
+  echo "Failed to build the Docker image."
+  # Restore app data from the temporary volume to rollback changes
+  restore_app_data
+  exit 1
+fi
+
+# Run the Docker container with the fixed port 3000
+run_container "$docker_image_name-container" "$1"
+
+# Clean up the temporary volume
+docker volume rm "$temp_volume_name"
+
